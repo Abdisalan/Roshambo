a94da8831fdeb459739f09ea8d459ee605f7810b
Abdisalan Mohamud <abdi.mohamud94@gmail.com>
2016-05-27 17:45:13 -0400

    Initial Commit

---
+++ RoshamboTests/RoshamboTests.swift
@@ -0,0 +1,36 @@
+//
+//  RoshamboTests.swift
+//  RoshamboTests
+//
+//  Created by Abdisalan Mohamud on 5/27/16.
+//  Copyright © 2016 Abdisalan Mohamud. All rights reserved.
+//
+
+import XCTest
+@testable import Roshambo
+
+class RoshamboTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
